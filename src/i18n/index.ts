dc8ef2632e7cc40d00e021ca0fd5318fa40fc565
Joao Felipe <joao.felipe.alves.m@gmail.com>
2026-02-25 00:41:46 -0300

    feat: add internationalization support with language resources for English, Spanish, and Portuguese

---
+++ src/i18n/index.ts
@@ -0,0 +1,43 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import ptCommon from "./resources/pt/common.json";
+import enCommon from "./resources/en/common.json";
+import esCommon from "./resources/es/common.json";
+
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "pt-BR",
+    debug: false,
+    ns: ["common"],
+    defaultNS: "common",
+    interpolation: {
+      escapeValue: false,
+    },
+    supportedLngs: ["en", "pt", "es"],
+    nonExplicitSupportedLngs: true,
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+    resources: {
+      en: {
+        common: enCommon,
+      },
+      pt: {
+        common: ptCommon,
+      },
+      es: {
+        common: esCommon,
+      },
+    },
+  })
+  .catch((error: unknown) => {
+    console.error("i18n init failed:", error);
+  });
+
+export default i18n;
